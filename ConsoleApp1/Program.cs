180bd608cd7ecbfb8183dd2fd32116fa8192c584
warriorcat34 <finndips279@gmail.com>
2024-03-26 20:27:18 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Banned words: ");
+            string[] banWords = Console.ReadLine().Split(' ').ToArray(); 
+
+            Console.WriteLine("Type a sentance: ");
+            string text = Console.ReadLine();
+
+            foreach (var banWord in banWords)
+            {
+                if (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord,
+                    new string('*', banWord.Length));
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ SHAKE ITTTT/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SHAKE_ITTTT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("pattern: ");
+            string pattern = Console.ReadLine();
+            bool success = ShakeString(ref str, pattern);
+
+            if (success)
+            {
+                Console.WriteLine("Shaked it.");
+            }
+            else
+            {
+                Console.WriteLine("No shake.");
+                Console.WriteLine(str);
+            }
+        }
+
+        static bool ShakeString(ref string str, string pattern)
+        {
+            int patternLength = pattern.Length;
+            bool success = false;
+
+            while (true)
+            {
+                int firstIndex = str.IndexOf(pattern);
+                int lastIndex = str.LastIndexOf(pattern);
+
+                if (firstIndex != -1 && lastIndex != -1 && firstIndex != lastIndex)
+                {
+                    str = str.Remove(firstIndex, patternLength);
+                    str = str.Remove(lastIndex - patternLength + 1, patternLength);
+                    pattern = pattern.Remove(patternLength / 2, 1);
+                    success = true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return success;
+        }
+    }
+    
+}
